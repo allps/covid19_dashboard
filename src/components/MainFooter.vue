40377a00370f73cf6ce1bec1b2566e686b956da4
Schartz Rehan <rehan.qt@gmail.com>
2020-04-02 00:31:19 +0530

    Update link in footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -2,7 +2,7 @@
     <footer class="footer">
         <div class="content has-text-centered">
             <p>
-                Covid-19 Stats Dashboard by <a href="https://jgthms.com">Science Fiction Lab.</a>.
+                Covid-19 Stats Dashboard by <a href="#">Science Fiction Lab.</a>.
                 <br>
                 Made possible with the support from good folks at <a href="https://allps.ch" target="_blank">Allps GmbH</a>.
                 <br>
